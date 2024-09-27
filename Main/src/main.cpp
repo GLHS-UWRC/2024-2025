45eadd21d5c9e5dcb08b49266eeafb9b7ed325dd
Awashcard0 <lumpy27@outlook.com>
2024-09-27 09:00:20 -0400

    Wow

--- Main/src/main.cpp
+++ Main/src/main.cpp
@@ -2,9 +2,10 @@
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  // hello
 }
