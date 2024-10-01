7a3ef618a9cd9cda159ae28d76def9cf8f5ec4ab
Awashcard0 <lumpy27@outlook.com>
2024-09-30 23:22:24 -0400

    Make start script and add UNO build config

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 #include <Arduino.h>
 
 void setup() {
-  // put your setup code here, to run once:
   
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
-  // Hello  
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
 }
