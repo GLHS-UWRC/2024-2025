2e3d64024602fafd489bf328116e36e8ab3905ff
Awashcard0 <lumpy27@outlook.com>
2024-10-27 21:44:42 -0400

    This thing wont upload :(

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,30 @@
 #include <Arduino.h>
 
+// Claw
+
 #define ClawSholder 0
 #define ClawElbow 1
 #define ClawGrip 2
+
+// Drive
 
 #define HorizontalLeft 3
 #define HorizontalRight 4
 #define VerticalLeft 5
 #define VerticalRight 6
+
+// Controls
+
+#define DriverLH 7
+#define DriverLV 8
+#define DriverRH 9
+#define DriverRV 10
+
+#define ArmJoy 11
+#define ElevJoy 12
+#define ClawOpen 13
+#define ClawClose 14
+
 
 void setup() {
   
